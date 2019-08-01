5cd1762e1536535d277ccf0bef2cfde1a489c0a4
brunolebrao <brunolebrao@ext.cvc.com.br>
2019-08-01 18:00:49 -0300

    Criar estrutura de projeto

---
+++ src/components/Board/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import List from '../List';
+import { Container } from './styles';
+
+export default function Board() {
+  return (
+    <Container>
+      <List/>
+    </Container>
+  );
+}
